9d29ab8aee7425c67bc65eef6f47ea31eee7a86e
jimlee <jim19911126@gmail.com>
2025-06-19 11:01:57 +0800

    json 抓資料練習

---
+++ self_20250619/s20250619_02_data.php
@@ -0,0 +1,13 @@
+<?php
+
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+$input = $_GET;
+dd($input);
+
+?>
---
+++ self_20250619/student/index_ajax.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+            width: 80%;
+            margin: auto;
+            text-align: center;
+        }
+
+        table,
+        th,
+        tr,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+
+<body>
+    <table id="myTable">
+        <tr>
+            <th>id</th>
+            <th>name</th>
+        </tr>
+        <!-- <tr>
+            <td>1</td>
+            <td>amy</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>amy</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>amy</td>
+        </tr> -->
+
+        <!-- 載入jquery -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+            integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <Script>
+            $(document).ready(function () {
+                const myTable = $('#myTable');
+
+    
+                let url = "./data/data.php";
+                $.ajax({
+                    type: "get",
+                    url: url,
+                    // data: "data",
+                    dataType: "json",
+                    success: function (response) {
+                        console.log('response', response);
+                        console.log('response typeof', typeof(response));
+                        let data = response;
+                        console.log('data', data);
+
+                        let tmpResult = '';
+                        $.each(data, function (key, value) {
+                            let tmpText = `
+                            <tr>
+                                <td>${value.id}</td>
+                                <td>${value.name}</td>
+                            </tr>
+                            `;
+                            tmpResult += tmpText;
+                 
+                             
+                        });
+                        
+                        myTable.append(tmpResult);
+                        
+
+
+
+                    }
+                });
+            });
+        </Script>
+
+</body>
+
+</html>
