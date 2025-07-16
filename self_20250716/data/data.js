293365f015df0d4e2730f2e1041e4867604ea644
jimlee <jim19911126@gmail.com>
2025-07-16 16:24:29 +0800

    單字小卡作業

---
+++ self_20250716/data/data.js
@@ -0,0 +1,137 @@
+let data=[
+  {
+    "id": 1,
+    "kanji": "約束",
+    "kana": "やくそく",
+    "romaji": "yakusoku",
+    "meaning_zh": "約定；約束",
+    "example_ja": "明日、10時に駅で約束があります。",
+    "example_zh": "明天十點在車站有約定。"
+  },
+  {
+    "id": 2,
+    "kanji": "経験",
+    "kana": "けいけん",
+    "romaji": "keiken",
+    "meaning_zh": "經驗",
+    "example_ja": "彼は海外での経験が豊富です。",
+    "example_zh": "他有豐富的海外經驗。"
+  },
+  {
+    "id": 3,
+    "kanji": "挑戦",
+    "kana": "ちょうせん",
+    "romaji": "chousen",
+    "meaning_zh": "挑戰",
+    "example_ja": "新しいことに挑戦するのが好きです。",
+    "example_zh": "我喜歡挑戰新的事物。"
+  },
+  {
+    "id": 4,
+    "kanji": "気持ち",
+    "kana": "きもち",
+    "romaji": "kimochi",
+    "meaning_zh": "心情；感受",
+    "example_ja": "彼女の気持ちが分からない。",
+    "example_zh": "我不懂她的心情。"
+  },
+  {
+    "id": 5,
+    "kanji": "努力",
+    "kana": "どりょく",
+    "romaji": "doryoku",
+    "meaning_zh": "努力",
+    "example_ja": "成功のために努力します。",
+    "example_zh": "為了成功而努力。"
+  },
+  {
+    "id": 6,
+    "kanji": "安心",
+    "kana": "あんしん",
+    "romaji": "anshin",
+    "meaning_zh": "安心；放心",
+    "example_ja": "無事に着いて安心しました。",
+    "example_zh": "平安到達讓我放心了。"
+  },
+  {
+    "id": 7,
+    "kanji": "理由",
+    "kana": "りゆう",
+    "romaji": "riyuu",
+    "meaning_zh": "理由",
+    "example_ja": "遅刻した理由を説明してください。",
+    "example_zh": "請說明遲到的理由。"
+  },
+  {
+    "id": 8,
+    "kanji": "習慣",
+    "kana": "しゅうかん",
+    "romaji": "shuukan",
+    "meaning_zh": "習慣",
+    "example_ja": "朝早く起きる習慣があります。",
+    "example_zh": "有早起的習慣。"
+  },
+  {
+    "id": 9,
+    "kanji": "目的",
+    "kana": "もくてき",
+    "romaji": "mokuteki",
+    "meaning_zh": "目的",
+    "example_ja": "目的を持って行動しましょう。",
+    "example_zh": "要有目的地行動吧。"
+  },
+  {
+    "id": 10,
+    "kanji": "関係",
+    "kana": "かんけい",
+    "romaji": "kankei",
+    "meaning_zh": "關係",
+    "example_ja": "彼とは良い関係です。",
+    "example_zh": "和他關係很好。"
+  },
+  {
+    "id": 11,
+    "kanji": "希望",
+    "kana": "きぼう",
+    "romaji": "kibou",
+    "meaning_zh": "希望",
+    "example_ja": "明るい未来を希望しています。",
+    "example_zh": "希望有光明的未來。"
+  },
+  {
+    "id": 12,
+    "kanji": "危険",
+    "kana": "きけん",
+    "romaji": "kiken",
+    "meaning_zh": "危險",
+    "example_ja": "あそこは危険だから近づかないでください。",
+    "example_zh": "那裡很危險，請不要靠近。"
+  },
+  {
+    "id": 13,
+    "kanji": "説明",
+    "kana": "せつめい",
+    "romaji": "setsumei",
+    "meaning_zh": "說明",
+    "example_ja": "詳しく説明してください。",
+    "example_zh": "請詳細說明。"
+  },
+  {
+    "id": 14,
+    "kanji": "発見",
+    "kana": "はっけん",
+    "romaji": "hakken",
+    "meaning_zh": "發現",
+    "example_ja": "新しい発見がありました。",
+    "example_zh": "有了新的發現。"
+  },
+  {
+    "id": 15,
+    "kanji": "感謝",
+    "kana": "かんしゃ",
+    "romaji": "kansha",
+    "meaning_zh": "感謝",
+    "example_ja": "ご協力に感謝します。",
+    "example_zh": "感謝您的協助。"
+  }
+]
