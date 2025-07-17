e75a0450d360379d03023736660bec7ff16eead6
jimlee <jim19911126@gmail.com>
2025-07-17 15:21:23 +0800

    PHP 物件導向練習

---
+++ self_20250717/playlist.php
@@ -0,0 +1,49 @@
+<?php
+$artpop = 'Gaga正在唱 ARTPOP 的 Applause';
+$mayhem = 'Gaga正在唱 Mayhem 的 LoveDrug';
+//  ..... more ......
+
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+// 水果 class
+class Setlist
+{
+    //properties
+    public $album;
+    public $song;
+    // private $color;
+
+
+    // methods
+    function play()
+    {
+        $text = "Gaga 正在唱 $this->album 的 $this->song<br>";
+        echo $text;
+    }
+}
+
+// 具象化
+// object
+$artpop = new Setlist();
+
+// apple.name
+// apple->name
+// apple['name']
+dd($artpop);
+
+$artpop->album = "ARTPOP";
+$artpop->song = "紅色";
+$artpop->play();
+
+
+$mayhem = new Setlist();
+$mayhem->album = "芒果";
+$mayhem->song = "黃色";
+$mayhem->play();
+
+
